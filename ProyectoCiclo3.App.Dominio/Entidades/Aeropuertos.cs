ec669d00371c5114791ac88154d7b3007a53953d
JohnFredy1 <johnvillalba@misena.edu.co>
2021-09-22 08:42:05 -0500

    commint v2

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Aeropuertos.cs
@@ -0,0 +1,12 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Aeropuertos{
+        public int id { get; set; }
+        public string Nombre{ get; set; }
+        public String Ciudad{ get; set; }
+	public String Pais{ get; set; }        
+	public int CoordX{ get; set; }
+        public int Coordy{ get; set; }
+    }
+ 
+}
---
+++ ProyectoCiclo3.App.Dominio/Entidades/Rutas.cs
@@ -0,0 +1,11 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Rutas{
+        public int id { get; set; }
+        public string Origne{ get; set; }
+        public String Destino{ get; set; }
+        public int TiempoEstimado{ get; set; }
+        
+    }
+ 
+}
