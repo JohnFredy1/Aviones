9c3ce6d24b8587e6ab0ba491fdf121330b09e9c4
JohnFredy1 <johnvillalba@misena.edu.co>
2021-09-22 23:53:47 -0500

    commint v3

--- ProyectoCiclo3.App.Dominio/Entidades/Rutas.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Rutas.cs
@@ -2,8 +2,8 @@ using System;
 namespace ProyectoCiclo3.App.Dominio{
     public class Rutas{
         public int id { get; set; }
-        public string Origne{ get; set; }
-        public String Destino{ get; set; }
+        public string Origen{ get; set; }
+        public string Destino{ get; set; }
         public int TiempoEstimado{ get; set; }
         
     }
