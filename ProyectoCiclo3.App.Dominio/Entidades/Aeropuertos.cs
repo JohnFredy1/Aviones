70955b64159295c1245f30986b74a466eb770f5d
JohnFredy1 <johnvillalba@misena.edu.co>
2021-09-23 22:41:04 -0500

    Modificacion v1

--- ProyectoCiclo3.App.Dominio/Entidades/Aeropuertos.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Aeropuertos.cs
@@ -3,9 +3,9 @@ namespace ProyectoCiclo3.App.Dominio{
     public class Aeropuertos{
         public int id { get; set; }
         public string Nombre{ get; set; }
-        public String Ciudad{ get; set; }
-	public String Pais{ get; set; }        
-	public int CoordX{ get; set; }
+        public string Ciudad{ get; set; }
+	    public string Pais{ get; set; }        
+	    public int CoordX{ get; set; }
         public int Coordy{ get; set; }
     }
  
